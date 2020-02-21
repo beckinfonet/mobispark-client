cac80569f0c9f0024f4625b404e82f8baf67440a
beckinfonet <bakytbek.tatibekov@gmail.com>
2020-02-20 20:51:34 -0500

    functions added

--- spec/description.js
+++ spec/description.js
@@ -3,3 +3,28 @@ describe('addition', function() {
     expect(addNumbers(2, 5)).toEqual(7);
   });
 })
+
+describe('multiplication', function() {
+  it('should multiply two valid integers and return a valid result', function() {
+    expect(multiplyNumbers(2, 5)).toEqual(10);
+  });
+})
+
+describe('division', function() {
+  it('should divide integer by another integer', function() {
+    expect(divideNumbers(20, 5)).toEqual(4);
+  });
+})
+
+
+describe('division', function() {
+  it('should substract integer from another integer', function() {
+    expect(substractNumber(20, 5)).toEqual(15);
+  });
+})
+
+// describe('if drinking age is legal, it should return true', function() {
+//   it('should substract integer from another integer', function() {
+//     expect(legalAge(result)).toBeGreaterThan(21);
+//   });
+// })
--- src/functions.js
+++ src/functions.js
@@ -1,3 +1,15 @@
 function addNumbers(a, b) {
   return a + b;
 }
+
+function multiplyNumbers(a, b) {
+  return a * b;
+}
+
+function divideNumbers(a, b) {
+  return a / b;
+}
+
+function substractNumber(a, b) {
+  return a - b;
+}
