f083e9f467427433a97faf48483e96e2b7ded141
Bakytbek Tatibekov <bakytbek.tatibekov@gmail.com>
2023-04-20 23:52:44 -0700

    added a loader

--- src/App.js
+++ src/App.js
@@ -3,17 +3,26 @@ import { Amplify } from "aws-amplify";
 import { useAuthenticator, Authenticator } from "@aws-amplify/ui-react";
 import { AppRoutes } from "./AppRoutes";
 import awsExports from "./aws-exports";
+import Stack from "@mui/material/Stack";
+import CircularProgress from "@mui/material/CircularProgress";
 
 Amplify.configure(awsExports);
 
+const CircularColor = () => {
+  return (
+    <Stack sx={{ color: "grey.500" }} spacing={2} direction="row">
+      <CircularProgress color="success" />
+    </Stack>
+  );
+};
+
 function App() {
   const { authStatus } = useAuthenticator((context) => [context.authStatus]);
-  // console.log("App ....", authStatus);
   return (
     <>
       {authStatus === "configuring" ? (
         <>
-          <div>Loading ....</div>
+          <CircularColor />
           <Authenticator />
         </>
       ) : (
